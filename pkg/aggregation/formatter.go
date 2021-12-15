0008228ac3c25783aa470934ec0d217a6af4e9fc
Florent Biville <florent.biville@neo4j.com>
2021-12-15 18:16:52 +0100

    Implement sorting
    Sort by count first, then by feature/reason
    
    Fixes #9

--- pkg/aggregation/formatter.go
+++ pkg/aggregation/formatter.go
@@ -2,9 +2,9 @@ package aggregation
 
 import (
 	"fmt"
-
 	"github.com/fbiville/markdown-table-formatter/pkg/markdown"
 	. "github.com/fbiville/testkit-reporter/pkg/entity"
+	"strconv"
 )
 
 func FormatTableByTestCount(title, column1, column2 string, data map[string][]SkippedTest) string {
@@ -14,8 +14,8 @@ func FormatTableByTestCount(title, column1, column2 string, data map[string][]Sk
 		table[row] = []string{k, fmt.Sprintf("%d", len(v))}
 		row++
 	}
-
 	formattedTable, err := markdown.NewTableFormatterBuilder().
+		WithCustomSort(sortIntDesc(1), markdown.ASCENDING_ORDER.StringCompare(0)).
 		WithPrettyPrint().
 		Build(column1, column2).
 		Format(table)
@@ -25,4 +25,17 @@ func FormatTableByTestCount(title, column1, column2 string, data map[string][]Sk
 	}
 	return title + ":\n\n" + formattedTable
 
+}
+
+func sortIntDesc(column int) markdown.SortFunction {
+	return markdown.SortFunction{
+		Fn:     sortIntDescFn,
+		Column: column,
+	}
+}
+
+func sortIntDescFn(s1, s2 string) int {
+	i1, _ := strconv.Atoi(s1)
+	i2, _ := strconv.Atoi(s2)
+	return i2 - i1
 }
